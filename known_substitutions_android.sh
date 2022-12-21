c4d1cc4214667b5f916cb7b2e9cbce73c181913b
DroidFreak32 <rushabshah32@gmail.com>
2022-12-21 14:09:12 +0530

    rsync: Enable support for zstd compression
    Change-Id: Ia2f0eb6e837ae55c2746aa5a42e265f0fef11e67

--- known_substitutions_android.sh
+++ known_substitutions_android.sh
@@ -8,4 +8,5 @@ sed -i 's|/\* #undef HAVE_XXHASH_H \*/|#define HAVE_XXHASH_H 1|g;' config.h
 sed -i 's|/\* #undef MKNOD_CREATES_FIFOS \*/|#define MKNOD_CREATES_FIFOS 1|g;' config.h
 sed -i 's|/\* #undef SUPPORT_LZ4 \*/|#define SUPPORT_LZ4 1|g;' config.h
 sed -i 's|/\* #undef SUPPORT_XXHASH \*/|#define SUPPORT_XXHASH 1|g;' config.h
+sed -i 's|/\* #undef SUPPORT_ZSTD \*/|#define SUPPORT_ZSTD 1|g;' config.h
 sed -i 's|/\* #undef USE_OPENSSL \*/|#define USE_OPENSSL 1|g;' config.h
